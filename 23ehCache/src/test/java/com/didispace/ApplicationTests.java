5bfee3ae0b0a5652da56a92e59306fdaf409c2b1
zhengyingdeng <892467242@qq.com>
2018-06-15 17:11:11 +0800

    EhCache缓存

---
+++ 23ehCache/src/test/java/com/didispace/ApplicationTests.java
@@ -0,0 +1,45 @@
+package com.didispace;
+
+import com.didispace.domain.User;
+import com.didispace.domain.UserRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+public class ApplicationTests {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Before
+	public void before() throws Exception {
+
+		// 创建10条记录
+		userRepository.save(new User("AAA", 10));
+		userRepository.save(new User("BBB", 20));
+		userRepository.save(new User("CCC", 30));
+		userRepository.save(new User("DDD", 40));
+		userRepository.save(new User("EEE", 50));
+		userRepository.save(new User("FFF", 60));
+		userRepository.save(new User("GGG", 70));
+		userRepository.save(new User("HHH", 80));
+		userRepository.save(new User("III", 90));
+		userRepository.save(new User("JJJ", 100));
+	}
+
+	@Test
+	public void query() throws Exception {
+		User u1 = userRepository.findByName("AAA");
+		System.out.println("第一次查询：" + u1.getAge());
+
+		User u2 = userRepository.findByName("AAA");
+		System.out.println("第二次查询：" + u2.getAge());
+	}
+}
